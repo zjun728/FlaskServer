3d7263699ca0e5c39395b3953801a14c3c649f00
zhangjun <503280155@qq.com>
2020-04-07 18:00:22 +0800

    1800

--- apps/views.py
+++ apps/views.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import render_template,request
 
 from apps import app
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def index():
+    if request.method=="POST":
+        fs=request.files["image_upload"]
+        if fs.filename!="":
+            fs.save(fs.filename)
+            return render_template('index.html',msg="上传成功")
     return render_template('index.html')
